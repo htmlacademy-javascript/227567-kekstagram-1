256947492ee9853fc50513a0ca015db186dc0181
Keks <keks@htmlacademy.ru>
2024-09-18 21:00:09 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/thumbnail.js
@@ -0,0 +1,34 @@
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+//const container = document.querySelector('.pictures');
+const createThumbnail = (picture) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = picture.url;
+  thumbnail.querySelector('.picture__img').alt = picture.deskription;
+  thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+  thumbnail.dataset.thumbnailId = picture.id;
+  return thumbnail;
+};
+
+const renderThumbnails = (pictures, container) => {
+  container.querySelectorAll('.picture').forEach((element) => element.remove());
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+
+
+  container.append(fragment);
+};
+/*
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+  container.append(fragment);
+};
+*/
+export {renderThumbnails};
